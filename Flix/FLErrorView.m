9d4c4afa9624e22de0534c0129835cd1d37afd21
parry <parry@parrys-air.attlocal.net>
2016-10-16 16:41:42 -0700

    most of stories done

--- Flix/FLErrorView.m
+++ Flix/FLErrorView.m
@@ -8,14 +8,55 @@
 
 #import "FLErrorView.h"
 
+@interface FLErrorView ()
+@property(strong,readwrite,nonatomic) UILabel *titleLabel;
+@property(strong,readwrite,nonatomic) UIImageView *errorImageView;
+
+
+@end
+
+
 @implementation FLErrorView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self.errorImageView = [[UIImageView alloc]init];
+    self.titleLabel = [[UILabel alloc]init];
+    
+    
+    if (!(self = [super initWithFrame:frame]))
+        return nil;
+    
+    
+    [self addSubview:self.errorImageView];
+    [self addSubview:self.titleLabel];
+    
+    
+//    [self setupActivityIndicator];
+    
+    return self;
 }
-*/
+
+
+-(void) layoutSubviews {
+    [super layoutSubviews];
+    
+    UILayoutGuide *margins = self.layoutMarginsGuide;
+    
+    self.errorImageView.translatesAutoresizingMaskIntoConstraints = false;
+    [self.errorImageView.leadingAnchor constraintEqualToAnchor:margins.leadingAnchor].active = YES;
+    [self.errorImageView.topAnchor constraintEqualToAnchor:margins.topAnchor].active = YES;
+    [self.errorImageView.bottomAnchor constraintEqualToAnchor:margins.bottomAnchor].active = YES;
+    self.errorImageView.contentMode = UIViewContentModeScaleAspectFit;
+    self.errorImageView.image = [UIImage imageNamed:@"errorViewIcon"];
+    
+    
+    self.titleLabel.translatesAutoresizingMaskIntoConstraints = false;
+    [self.titleLabel.leadingAnchor constraintEqualToAnchor:self.errorImageView.trailingAnchor constant:-10].active = YES;
+    [self.titleLabel.topAnchor constraintEqualToAnchor:margins.topAnchor].active = YES;
+    //    [self.titleLabel.bottomAnchor constraintEqualToAnchor:self.overviewLabel.topAnchor constant:5].active = YES;
+    self.titleLabel.font = [UIFont fontWithName:@"Avenir-Book" size:13];
+    self.titleLabel.textAlignment = NSTextAlignmentLeft;}
 
 @end
