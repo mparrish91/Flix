f0bfa17e124cd5e89aaf3f53eb2e46f79f29366a
parry <parry@parrys-air.attlocal.net>
2016-10-17 11:04:01 -0700

    backing up3

--- Flix/FLMovieTableViewCell.m
+++ Flix/FLMovieTableViewCell.m
@@ -51,6 +51,8 @@
     
     self.titleLabel.translatesAutoresizingMaskIntoConstraints = false;
     [self.titleLabel.leadingAnchor constraintEqualToAnchor:self.photoImageView.trailingAnchor constant:-10].active = YES;
+    [self.titleLabel.trailingAnchor constraintLessThanOrEqualToAnchor:margins.trailingAnchor].active = YES;
+
     [self.titleLabel.topAnchor constraintEqualToAnchor:margins.topAnchor].active = YES;
 //    [self.titleLabel.bottomAnchor constraintEqualToAnchor:self.overviewLabel.topAnchor constant:5].active = YES;
     self.titleLabel.font = [UIFont fontWithName:@"Avenir-Book" size:13];
@@ -59,16 +61,12 @@
     
     self.overviewLabel.translatesAutoresizingMaskIntoConstraints = false;
     [self.overviewLabel.leadingAnchor constraintEqualToAnchor:self.photoImageView.trailingAnchor constant:-10].active = YES;
-//    [self.overviewLabel.trailingAnchor constraintEqualToAnchor:margins.trailingAnchor].active = YES;
+    [self.overviewLabel.trailingAnchor constraintEqualToAnchor:margins.trailingAnchor].active = YES;
 
     [self.overviewLabel.topAnchor constraintEqualToAnchor:self.titleLabel.bottomAnchor constant:5].active = YES;
-//    [self.overviewLabel.bottomAnchor constraintEqualToAnchor:margins.bottomAnchor].active = YES;
+    [self.overviewLabel.bottomAnchor constraintGreaterThanOrEqualToAnchor:margins.bottomAnchor constant:0].active = YES;
     [self.overviewLabel sizeToFit];
 
-//    self.titleLabel.textAlignment = NSTextAlignmentCenter;
-//
-//
-//    
     self.overviewLabel.font = [UIFont fontWithName:@"Avenir-Book" size:9];
     self.overviewLabel.textColor = [UIColor grayColor];
     self.overviewLabel.numberOfLines = 0;
